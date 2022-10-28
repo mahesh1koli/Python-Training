fa9806bb9ce9d1f969236d34d3f22248604f1303
Mahesh Kolisetti <52033735+mahesh1koli@users.noreply.github.com>
2022-10-28 12:39:12 +0530

    Add files via upload

---
+++ July 2022 Batch-2/filehandling.py
@@ -0,0 +1,5 @@
+"""This function is to open a file and read content"""
+f = open(r'C:\Users\mahes\OneDrive\Desktop\Anaconda_silent_install_options.txt',
+         encoding='utf-8')
+print(f.read())
+f.close()
